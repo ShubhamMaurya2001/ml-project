c5dd8ef4bb61b95448cc156992f3d48590f8900e
Shubham Maurya <shubham2k@outlook.com>
2025-12-11 11:31:27 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 from setuptools import setup, find_packages
+from typing import List
+
+hyphen_e_dot = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    function to fetch for requirements
+    
+    :param file_path: Description
+    :type file_path: str
+    :return: Description
+    :rtype: Any
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n","") for req in requirements]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
 
 setup(
     
@@ -7,5 +27,5 @@ setup(
     author = "Shubham",
     author_email = "shubham2k@outlook.com",
     packages = find_packages(),
-    install_requires = ['pandas','numpy','seaborn']
+    install_requires = get_requirements('requirements.txt')
 )
